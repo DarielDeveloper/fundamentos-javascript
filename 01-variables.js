58ff31647f8dc3107b7762e47d5f9f78af9474fa
Dariel <150453595+DarielDeveloper@users.noreply.github.com>
2025-05-25 17:46:17 -0400

    variables y tipos

---
+++ 01-variables.js
@@ -0,0 +1,31 @@
+/*
+Clase 16 - Variables
+Vídeo: https://youtu.be/1glVfFxj8a4?t=3049
+*/
+
+// var fue la forma original de declarar variable. OJO NO ES RECOMENDABLE 
+
+var helloWorld = "¡Hola, JavaScript!"
+console.log(helloWorld)
+
+helloWorld = "¡Hola de nuevo, JavaScript!"
+console.log(helloWorld)
+
+// LO recomendado para declarar son let y const
+
+// let se puede setaer
+
+let helloWorld2 = "¡Hola, JavaScript 2!"
+console.log(helloWorld2)
+
+helloWorld2 = "¡Hola de nuevo, JavaScript 2!"
+console.log(helloWorld2)
+
+// const
+
+const helloWorld3 = "¡Hola, JavaScript 3!"
+console.log(helloWorld3)
+
+// Error porque no se puede setaer, pero cuidado si se puede mutar
+// helloWorld3 = "¡Hola de nuevo, JavaScript 2!"
+// console.log(helloWorld3)
